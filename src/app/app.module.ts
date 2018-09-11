6d1cf8d51d89b108245a0cd6582516054fe36fb7
floraFoodi <flora.qian@besprouttech.com>
2018-09-11 10:52:26 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BottomComponent } from './bottom/bottom.component';
+import { ImgsliderComponent } from './imgslider/imgslider.component';
+import { TopComponent } from './top/top.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { WhyisudsComponent } from './whyisuds/whyisuds.component';
+import { SuccessstoryComponent } from './successstory/successstory.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { TestdisplayComponent } from './testdisplay/testdisplay.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BottomComponent,
+    ImgsliderComponent,
+    TopComponent,
+    HomeComponent,
+    ProductComponent,
+    WhyisudsComponent,
+    SuccessstoryComponent,
+    ContactusComponent,
+    TestdisplayComponent
   ],
   imports: [
     BrowserModule,
